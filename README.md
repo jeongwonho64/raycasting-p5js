0237a4492ac3fec7e81c07db73d58671fbfb73a9
jeongwonho64 <attemptn63@gmail.com>
2024-07-22 23:06:51 +0800

    done
    added particle, finished raytracing, added instruction

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # raycasting-p5js
-Small raycasting simulation on p5js
+Small raycasting simulation on p5js  
+Use A and D to rotate, arrow keys to move
--- sketch.js
+++ sketch.js
@@ -32,6 +32,71 @@ class Ray {
     }
 }
 
+class Particle {
+    constructor() {
+        this.pos = createVector(width / 2, height / 2);
+        this.velocity = createVector(0, 0);
+        this.rays = [];
+        // Change the restrictions of a for more/less rays
+        for (let a = -20; a <= 20; a += 1) {
+            this.rays.push(new Ray(this.pos, a));
+        }
+    }
+
+    update() {
+        this.pos.add(this.velocity);
+        for (let ray of this.rays) {
+            ray.pos = this.pos.copy();
+        }
+    }
+
+    move(x, y) {
+        this.velocity.x = x;
+        this.velocity.y = y;
+    }
+
+    stopMoving() {
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+    }
+
+    turn(angle) {
+        for (let ray of this.rays) {
+            ray.dir.rotate(angle);
+        }
+    }
+
+    lookAt(walls) {
+        const hits = [];
+        for (let ray of this.rays) {
+            let closest = null;
+            let record = Infinity;
+            for (let wall of walls) {
+                const pt = ray.cast(wall);
+                if (pt) {
+                    const d = p5.Vector.dist(this.pos, pt);
+                    if (d < record) {
+                        record = d;
+                        closest = pt;
+                    }
+                }
+            }
+            hits.push(closest);
+        }
+        return hits;
+    }
+
+    show(walls) {
+        for (let i = 0; i < this.rays.length; i++) {
+            const closest = this.lookAt(walls)[i];
+            if (closest) {
+                stroke(255);
+                line(this.pos.x, this.pos.y, closest.x, closest.y);
+            }
+        }
+    }
+}
+
 class Wall {
     constructor(x1, y1, x2, y2) {
         this.a = createVector(x1, y1); // Start point of the wall
@@ -44,7 +109,9 @@ class Wall {
     }
 }
 
+let particle;
 let walls = [];
+let turnSpeed = 0.1;
 
 function setup() {
     createCanvas(500, 500);
@@ -57,9 +124,42 @@ function setup() {
     walls.push(new Wall(0, 0, 500, 0));
     walls.push(new Wall(500, 500, 0, 500));
     walls.push(new Wall(500, 500, 500, 0));
+
+    particle = new Particle();
 }
 
 function draw() {
     background(0);
+
+    particle.update();
+  
+    for (let wall of walls) {
+        wall.show();
+    }
+
+    particle.show(walls);
 }
 
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        particle.move(-1, 0);
+    } else if (keyCode === RIGHT_ARROW) {
+        particle.move(1, 0);
+    } else if (keyCode === UP_ARROW) {
+        particle.move(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        particle.move(0, 1);
+    } else if (key === 'A' || key === 'a') {
+        particle.turn(-turnSpeed);
+    } else if (key === 'D' || key === 'd') {
+        particle.turn(turnSpeed);
+    }
+}
+
+function keyReleased() {
+    if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW || keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+        particle.stopMoving();
+    }
+}
+
+    
